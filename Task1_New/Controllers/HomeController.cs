33f6a4d7eee88711b9458fb5be137f6385793e5e
Sanjay Kumar Jaiswal <sanjayjaiswal9818621124@gmail.com>
2024-10-24 13:41:26 +0530

    post edit action Updated

--- Task1_New/Controllers/HomeController.cs
+++ Task1_New/Controllers/HomeController.cs
@@ -67,54 +67,6 @@ namespace Task1_New.Controllers
             return View(viewModel);
         }
 
-
-
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Edit(int id, School viewModel)
-        //{
-
-        //    if (id != viewModel.StudentInfo.StudentId)
-        //    {
-        //        return NotFound();
-        //    }
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Students.Update(viewModel.StudentInfo);
-        //        //await db.SaveChangesAsync();
-        //        // Set StudentId for each Parent
-        //        foreach (var parent in viewModel.ParentsList)
-        //        {
-        //            var prnt = await db.Parents.Where(x => x.Id == parent.Id).FirstOrDefaultAsync();
-        //            if (prnt != null)
-        //            {
-        //                prnt.StudentId = id;
-        //                prnt.Name = parent.Name;
-        //                prnt.Contact = parent.Contact;
-        //                db.Entry(prnt).State = EntityState.Modified;
-        //                db.Parents.Update(prnt);
-        //            }
-        //            else
-        //            {
-        //                var pare = new ParentInfo()
-        //                {
-        //                    Name = parent.Name,
-        //                    StudentId = viewModel.StudentInfo.StudentId,
-        //                    Contact = parent.Contact
-        //                };
-        //                db.Parents.Add(pare);
-
-        //            }
-        //            //db.Parents.Update(pare);
-        //        }
-        //        await db.SaveChangesAsync();
-
-        //        return RedirectToAction(nameof(Index));
-        //    }
-
-        //    return View(viewModel);
-        //}
-
         public IActionResult create()
         {
             var viewModel = new School
@@ -211,180 +163,6 @@ namespace Task1_New.Controllers
 
 
 
-
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Edit(int? id, School viewModel)
-        //{
-
-        //    if (id != viewModel.StudentInfo.StudentId)
-        //    {
-        //        return NotFound();
-        //    }
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Students.Update(viewModel.StudentInfo);
-        //        // Set StudentId for each Parent
-        //        foreach (var parent in viewModel.ParentsList)
-        //        {
-        //            if (parent != null)
-        //            {
-
-        //                var pare = new ParentInfo()
-        //                {
-        //                    Name = parent.Name,
-        //                    StudentId = viewModel.StudentInfo.StudentId,
-        //                    Contact = parent.Contact
-        //                };
-        //                db.Parents.Update(pare);
-        //            }
-        //        }
-        //        await db.SaveChangesAsync();
-
-        //        return RedirectToAction(nameof(Index));
-        //    }
-
-        //    return View(viewModel);
-        //}
-
-
-
-
-        ////Chatgpt 1
-
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Edit(int id, School viewModel)
-        //{
-        //    if (id != viewModel.StudentInfo.StudentId)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Students.Update(viewModel.StudentInfo);
-
-        //        // Get the current list of parents in the database for this student
-        //        var existingParents = await db.Parents.Where(p => p.StudentId == id).ToListAsync();
-
-
-        //        // Remove parents that are not in the updated list
-        //        foreach (var parent in existingParents)
-        //        {
-        //            if (!viewModel.ParentsList.Any(p => p.Id == parent.Id))
-        //            {
-        //                db.Parents.Remove(parent);
-        //            }
-        //        }
-
-        //        // Update or add parents
-        //        foreach (var parent in viewModel.ParentsList)
-        //        {
-        //            var existingParent = existingParents.FirstOrDefault(p => p.Id == parent.Id);
-
-        //            if (existingParent != null)
-        //            {
-        //                // Update existing parent
-        //                existingParent.Name = parent.Name;
-        //                existingParent.Contact = parent.Contact;
-        //                db.Entry(existingParent).State = EntityState.Modified;
-        //            }
-        //            else
-        //            {
-        //                // Add new parent
-        //                var newParent = new ParentInfo
-        //                {
-        //                    Name = parent.Name,
-        //                    StudentId = id,
-        //                    Contact = parent.Contact
-        //                };
-        //                db.Parents.Add(newParent);
-        //            }
-        //        }
-
-
-
-        //        await db.SaveChangesAsync();
-        //        return RedirectToAction(nameof(Index));
-        //    }
-
-        //    return View(viewModel);
-        //}
-
-
-
-
-        ////Chatgpt2
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Edit(int id, School viewModel)
-        //{
-        //    if (id != viewModel.StudentInfo.StudentId)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    if (ModelState.IsValid)
-        //    {
-        //        // Update the student info
-        //        db.Students.Update(viewModel.StudentInfo);
-
-        //        // Retrieve all current parents for the student from the database
-        //        var existingParents = await db.Parents.Where(p => p.StudentId == id).ToListAsync();
-
-        //        // Create a list to keep track of parent IDs that should not be deleted
-        //        var parentIdsToKeep = new HashSet<int>();
-
-        //        // Loop through the parents from the viewModel
-        //        foreach (var parent in viewModel.ParentsList)
-        //        {
-        //            if (parent.Id != null) // Existing parent
-        //            {
-        //                var existingParent = existingParents.FirstOrDefault(p => p.Id == parent.Id);
-        //                if (existingParent != null)
-        //                {
-        //                    // Update the existing parent's details
-        //                    existingParent.Name = parent.Name;
-        //                    existingParent.Contact = parent.Contact;
-        //                    db.Entry(existingParent).State = EntityState.Modified;
-
-        //                    // Add the parent ID to the list of IDs to keep
-        //                    parentIdsToKeep.Add(existingParent.Id);
-        //                }
-        //            }
-        //            else // New parent
-        //            {
-        //                var newParent = new ParentInfo
-        //                {
-        //                    Name = parent.Name,
-        //                    StudentId = id,
-        //                    Contact = parent.Contact
-        //                };
-        //                db.Parents.Add(newParent);
-        //            }
-        //        }
-
-        //        // Remove parents that are not in the list of parent IDs to keep
-        //        foreach (var parent in existingParents)
-        //        {
-        //            if (!parentIdsToKeep.Contains(parent.Id))
-        //            {
-        //                db.Parents.Remove(parent);
-        //            }
-        //        }
-
-        //        //Save all changes to the database
-        //        await db.SaveChangesAsync();
-        //        return RedirectToAction(nameof(Index));
-        //    }
-
-        //    return View(viewModel);
-        //}
-
-
-        //ChatGpt 3
-
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, School viewModel, string? DeletedParents)
@@ -401,30 +179,14 @@ namespace Task1_New.Controllers
 
                 // Get the current list of parents in the database for this student
                 var existingParents = await db.Parents.Where(p => p.StudentId == id).ToListAsync();
-
-                // Remove parents that are in the DeletedParents list
-                if (!string.IsNullOrEmpty(DeletedParents))
-                {
-                    var parentIdsToRemove = DeletedParents.Split(',')
-                                                          .Where(id => !string.IsNullOrWhiteSpace(id))
-                                                          .Select(int.Parse)
-                                                          .ToList();
-
-                    foreach (var parentId in parentIdsToRemove)
-                    {
-                        var parentToRemove = existingParents.FirstOrDefault(p => p.Id == parentId);
-                        if (parentToRemove != null)
-                        {
-                            db.Parents.Remove(parentToRemove);
-                        }
-                    }
-                }
-
+                db.Parents.RemoveRange(existingParents);
+                
                 // Update or add parents from the form
                 foreach (var parent in viewModel.ParentsList)
                 {
                     if (parent != null)
                     {
+                        // Check if the parent already exists in the database (using the Id field)
                         var existingParent = existingParents.FirstOrDefault(p => p.Id == parent.Id);
 
                         if (existingParent != null)
@@ -434,7 +196,7 @@ namespace Task1_New.Controllers
                             existingParent.Contact = parent.Contact;
                             db.Entry(existingParent).State = EntityState.Modified;
                         }
-                        else
+                        else if (parent.Id == 0) // Check if it's a new parent (Id == 0 or null for new entries)
                         {
                             // Add new parent
                             var newParent = new ParentInfo
@@ -454,12 +216,6 @@ namespace Task1_New.Controllers
 
             return View(viewModel);
         }
-
-
-
-
-
-
 
 
         public IActionResult Privacy()
